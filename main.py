023c652b73edac3e3ec7184b0c7319983f917820
Salted <44394537+xian18@users.noreply.github.com>
2022-02-03 14:03:57 +0800

    ..

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from fastapi import Depends, FastAPI, Header, Request
+from fastapi import Depends, FastAPI, Header, Request, HTTPException
 from fastapi.staticfiles import StaticFiles
 import uvicorn
-from typing import Optional
 from verfy import verfy_token
 
 app = FastAPI()
@@ -11,9 +10,13 @@ app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
 @app.post("/push_event", dependencies=[Depends(verfy_token)])
 async def push_event(X_Github_Event: str = Header(None), request: Request = None):
-    print(X_Github_Event)
-    print(request)
-    return {"status": "ok"}
+    if X_Github_Event == "push":
+        return {"message": "Push event received"}
+    elif X_Github_Event == "ping":
+        return {"message": "Ping event received"}
+    else:
+        raise HTTPException(
+            status_code=403, detail="X-Github-Event not supported")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
