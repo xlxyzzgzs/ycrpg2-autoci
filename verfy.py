e71ea401d4ee268474950b3d66d2317fe7a87163
ubuntu <ubuntu@localhost.localdomain>
2022-02-04 18:08:54 +0800

    ..

--- verfy.py
+++ verfy.py
@@ -7,6 +7,8 @@ import config
 
 async def verfy_token(X_Hub_Signature_256: str = Header(None), request: Request = None):
     body = await request.body()
+    if X_Hub_Signature_256.startswith('sha256='):
+        X_Hub_Signature_256 = X_Hub_Signature_256[7:]
     result = hmac.compare_digest(X_Hub_Signature_256, hmac.new(
         config.SECRET_KEY.encode(), body, hashlib.sha256).hexdigest())
     if not result:
